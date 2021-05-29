8714c74e2ab9b112746f6126620e2440e6634511
manish <manishk@winjit.com>
2021-05-29 17:30:45 +0530

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by manish on 29/05/21.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
